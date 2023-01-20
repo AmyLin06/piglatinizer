b084bbb1149f82010db2bab6f9c7e2d4a5d409ed
Amy <AmyLin1928@gmail.com>
2023-01-20 14:11:11 -0500

    added more UI features, cleaner javascript

--- js/script.js
+++ js/script.js
@@ -1,25 +1,60 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
-  $("#button").click(function(){
-      var input = $(".input").val();
-      pigWord(input);
-  });
-
-function pigWord(pig){
-    let vowel = ["a" , "e" , "i" , "o" , "u"];
-    let firstLetter = pig[0];
-    let newword = pig.substring(0);
-    vowel.forEach(function(vowels){
-        if (firstLetter === vowels){
-            $(".output").html(newword + pig[0] + "ay")
-        }
-        else{
-            $(".output").html(pig + "ay")
-        }
+    $("#button").click(function(){
+        var input = $(".input").val();
+        pigWord(input);
     });
-};
 
+    $("#clearTranslation").click(function(){
+        $("#engOutput").empty();
+        $("#latOutput").empty();
+    });
+    
+    function pigWord(pig){
+        let vowels = ["a" , "e" , "i" , "o" , "u"];
+        let firstLetter = pig[0];
 
+        //word starts with a vowel
+        for(const vowel of vowels){
+            if (firstLetter === vowel){
+                let latOutcome = $("<p></p>").text(pig + "yay");
+                $("#latOutput").append(latOutcome);
+                engAppend(pig);
+                return;
+            }
+        };
 
+        //word starts with a consonant or consonant cluster
+        let consoClus = consonantCluster(pig);
+        firstLetter = pig.substring(0, consoClus);
+        let newWord = pig.substring(consoClus);
+
+        let latOutcome = $("<p></p>").text(newWord + firstLetter + "ay");
+        $("#latOutput").append(latOutcome);
+        engAppend(pig);
+    };
+    
+    function consonantCluster(pig){
+        let consonantCount = 0;
+        let vowels = ["a" , "e" , "i" , "o" , "u"];
+
+        for(const letter of pig){
+            for(const vowel of vowels){
+                if(letter === vowel){
+                    return consonantCount;
+                }
+                if(letter === 'y' && consonantCount > 0){
+                    return consonantCount;
+                }
+            }
+            consonantCount++;
+        };
+        return consonantCount;
+    };
+    
+    function engAppend(pig){
+        let engOutcome = $("<p></p>").text(pig);
+        $("#engOutput").append(engOutcome);
+    }
 
 });
